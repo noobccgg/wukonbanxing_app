2d1e226379ab5713193c53782979448a6d36c2f0
Liu Hai <231300081@smail.nju.edu.cn>
2025-08-30 22:43:26 +0800

    Init icon tips, but fk android fades, try to fix it.

--- app/src/main/java/com/example/mnnllmdemo/ObstacleType.java
+++
@@ -1,9 +0,0 @@
-package com.example.mnnllmdemo;
-
-public enum ObstacleType {
-    // --- 你可以根据你的图标资源来定义这些 ---
-    NONE,                // 无特殊障碍
-    OBSTACLE_AHEAD,      // 正前方有障碍
-
-    WARNING              // 通用警告 (路障, 车辆)
-}
